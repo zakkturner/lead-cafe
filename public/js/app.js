746ceab9045a2def0df302bef3eaf737aa576cac
Zak Tee <zakkturner@yahoo.com>
2022-10-17 20:18:20 -0400

    deploying

--- public/js/app.js
+++ public/js/app.js
@@ -10892,7 +10892,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.list-group-item[data-v-0dde1eb2] {\n    display: flex;\n    flex-direction: column;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.list-group-item[data-v-0dde1eb2] {\r\n    display: flex;\r\n    flex-direction: column;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
