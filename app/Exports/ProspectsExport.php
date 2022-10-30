955a30c85b1221962c7d2cd849b226fdfb61c945
Zak Tee <zakkturner@yahoo.com>
2022-10-30 15:50:16 -0400

    export rough draft push

---
+++ app/Exports/ProspectsExport.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Prospect;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class ProspectsExport implements FromCollection, WithHeadings
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function headings(): array{
+        return[
+            'id',
+            'contact',
+            'company',
+            'phone_one',
+            'phone_two',
+            'address',
+            'web_url',
+            'email',
+            'position',
+            'notes',
+        ];
+    }
+
+    public function collection()
+    {
+        return Prospect::all();
+    }
+}
